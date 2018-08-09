502c098e9cfcbdbafea79222d3dae279ce62fcb4
Fabien Meghazi <agr@amigrave.com>
2018-08-09 14:40:46 +0200

    use valid manifest file

---
+++ fake_theme/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Fake Theme',
+    'description': 'Fake Theme',
+    'category': 'Theme/Ecommerce',
+    'sequence': 180,
+    'version': '1.0',
+    'author': 'Odoo S.A.',
+    'depends': [],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'price': 199,
+    'currency': 'EUR',
+    'live_test_url': 'https://example.com',
+}
